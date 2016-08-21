563248ca08451b90b8de268be26bc55007d970d0
Michael Kirschner <kirschm@BOSC02LW7TQFD57.local>
2016-08-21 00:01:15 -0400

    first commit including ignore file

---
+++ StateMachine/StateMachine/NodeModels.cs
@@ -0,0 +1,112 @@
+﻿using Dynamo.Graph.Nodes;
+using ProtoCore.AST.AssociativeAST;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMDataBridge;
+
+namespace StateMachine
+{
+    [NodeName("SetCurrentState")]
+    [NodeCategory("FiniteStateMachine")]
+    [IsDesignScriptCompatible]
+    public class SetCurrentState : NodeModel
+    {
+        public static string currentState { get; private set; }
+        public Action Executed;
+
+        public SetCurrentState()
+        {
+            Executed = new Action(() => { });
+            InPortData.Add(new PortData("state", "the name of the state to switch to"));
+
+            RegisterAllPorts();
+            CanUpdatePeriodically = true;
+
+        }
+        //when this node is executed it needs to schedule other nodes in the graph
+        //we don't actually care about the values generated here...
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            return new AssociativeNode[]
+            {
+                AstFactory.BuildAssignment(
+                    AstFactory.BuildIdentifier(AstIdentifierBase + "_dummy"),
+                    DataBridge.GenerateBridgeDataAst(GUID.ToString(), inputAstNodes.FirstOrDefault()))
+            };
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            DataBridge.Instance.UnregisterCallback(GUID.ToString());
+        }
+
+        protected override void OnBuilt()
+        {
+            base.OnBuilt();
+            DataBridge.Instance.RegisterCallback(GUID.ToString(), DataBridgeCallback);
+        }
+
+        private void DataBridgeCallback(object data)
+        {
+            SetCurrentState.currentState = data as string;
+            this.Executed();
+        }
+    }
+
+
+    [NodeName("OnCurrentState")]
+    [NodeCategory("FiniteStateMachine")]
+    [IsDesignScriptCompatible]
+    public class OnCurrentState : NodeModel
+    {
+        private object output;
+        public OnCurrentState()
+        {
+            InPortData.Add(new PortData("state", "the name of the state to activate on"));
+            InPortData.Add(new PortData("data", " data to pass through if state is active"));
+
+            OutPortData.Add(new PortData("data", "pass through data"));
+
+            RegisterAllPorts();
+            CanUpdatePeriodically = true;
+
+        }
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            return new AssociativeNode[]
+            {
+                //return the data if we execute
+                  AstFactory.BuildAssignment(
+                    GetAstIdentifierForOutputIndex(0),inputAstNodes[1])
+
+            };
+        }
+
+        //called from the UI thread using the engineMirror... may need to be scheduled...
+        public void SetFreezeState(string thisState)
+        {
+            try
+            {
+                //if the current state is the same as the one referenced by this node then pass the value out
+                //else return nothing...
+                if (SetCurrentState.currentState == thisState)
+                {
+                    this.IsFrozen = false;
+                }
+                else
+                {
+                    //freeze the node now.
+                    this.IsFrozen = true;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ StateMachine/StateMachine/NodeViewCustomizations.cs
@@ -0,0 +1,56 @@
+﻿using Dynamo.Controls;
+using Dynamo.Graph.Nodes;
+using Dynamo.Scheduler;
+using Dynamo.ViewModels;
+using Dynamo.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachine
+{
+    public class StatemachineCustomization : INodeViewCustomization<SetCurrentState>
+    {
+        private DynamoViewModel dynamoViewModel;
+        private NodeModel model;
+
+        public void Dispose()
+        {
+            //unhook handler
+            (this.model as SetCurrentState).Executed -= onExecuted;
+        }
+
+        public void CustomizeView(SetCurrentState model, NodeView nodeView)
+        {
+            dynamoViewModel = nodeView.ViewModel.DynamoViewModel;
+            this.model = model;
+            model.Executed += onExecuted;
+        }
+
+        private void onExecuted()
+        {
+            //find all nodes of type OnState
+            var nodesToExecute = dynamoViewModel.CurrentSpace.Nodes.OfType<OnCurrentState>();
+            var engine = dynamoViewModel.EngineController;
+            var stateString = "default";
+
+            nodesToExecute.ToList().ForEach(node => {
+                if (node.HasConnectedInput(0))
+                {
+                    //TODO(this needs to be made robust, what if data is not a string!?)
+                    var stateNode = node.InPorts[0].Connectors[0].Start.Owner;
+                    var stateIndex = node.InPorts[0].Connectors[0].Start.Index;
+                    var startId = stateNode.GetAstIdentifierForOutputIndex(stateIndex).Name;
+                    var stateMirror = engine.GetMirror(startId);
+                    stateString = stateMirror.GetData().Data as string;
+                }
+                var copy = stateString;
+                var task = new DelegateBasedAsyncTask(dynamoViewModel.Model.Scheduler, () => { node.SetFreezeState(copy); });
+                dynamoViewModel.Model.Scheduler.ScheduleForExecution(task);
+            });
+
+        }
+    }
+}
